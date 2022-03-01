f05d4e79cb74e61702a8bbd603f076c683a8029c
Daniikk1012 <daniyarsar@gmail.com>
2022-03-01 12:47:20 +0600

    Fix background scaling

--- src/background.rs
+++ src/background.rs
@@ -6,26 +6,30 @@ pub const BACKGROUND_LAYER: f32 = 0.0;
 pub const GAME_LAYER: f32 = 1.0;
 
 #[derive(Component)]
-pub struct Background;
+pub struct Background {
+    min_size: Vec2,
+}
 
 pub fn init(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let min_size = Vec2::new(1920.0, 1080.0);
+
     commands
         .spawn_bundle(SpriteBundle {
             sprite: Sprite {
-                custom_size: Some(Vec2::new(1920.0, 1080.0)),
+                custom_size: Some(Vec2::new(min_size.x, min_size.y)),
                 ..Default::default()
             },
             transform: Transform::from_xyz(960.0, 540.0, BACKGROUND_LAYER),
             texture: asset_server.load("background.png"),
             ..Default::default()
         })
-        .insert(Background);
+        .insert(Background { min_size });
 }
 
 pub fn resize(
     mut queries: QuerySet<(
         QueryState<(&Transform, &OrthographicProjection), With<GameCamera>>,
-        QueryState<(&mut Transform, &mut Sprite), With<Background>>,
+        QueryState<(&mut Transform, &mut Sprite, &Background)>,
     )>,
 ) {
     let (camera_transform, projection) = queries.q0().single();
@@ -37,8 +41,13 @@ pub fn resize(
         top: camera_transform.translation.y + projection.top,
     };
 
+    let camera_size = Vec2::new(
+        camera_bounds.right - camera_bounds.left,
+        camera_bounds.top - camera_bounds.bottom,
+    );
+
     let mut query = queries.q1();
-    let (mut transform, mut sprite) = query.single_mut();
+    let (mut transform, mut sprite, background) = query.single_mut();
 
     if transform.translation.x
         != (camera_bounds.left + camera_bounds.right) / 2.0
@@ -51,13 +60,23 @@ pub fn resize(
             (camera_bounds.bottom + camera_bounds.top) / 2.0;
     }
 
-    if sprite.custom_size.unwrap().x != camera_bounds.right - camera_bounds.left
-        || sprite.custom_size.unwrap().y
-            != camera_bounds.top - camera_bounds.bottom
+    let size = if camera_size.x * background.min_size.y
+        > background.min_size.x * camera_size.y
     {
-        sprite.custom_size = Some(Vec2::new(
-            camera_bounds.right - camera_bounds.left,
-            camera_bounds.top - camera_bounds.bottom,
-        ));
+        Vec2::new(
+            camera_size.x,
+            background.min_size.y * camera_size.x / background.min_size.x,
+        )
+    } else {
+        Vec2::new(
+            background.min_size.x * camera_size.y / background.min_size.y,
+            camera_size.y,
+        )
+    };
+
+    if sprite.custom_size.unwrap().x != size.x
+        || sprite.custom_size.unwrap().y != size.y
+    {
+        sprite.custom_size = Some(size);
     }
 }
