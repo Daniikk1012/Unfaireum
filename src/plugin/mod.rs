847d8c695caba82484647093314aae38ccfe8a67
Daniikk1012 <daniyarsar@gmail.com>
2022-03-05 01:30:44 +0600

    Fix web screen size

--- src/plugin/mod.rs
+++ src/plugin/mod.rs
@@ -10,6 +10,10 @@ pub mod camera;
 
 impl Plugin for MainPlugin {
     fn build(&self, app: &mut App) {
+        #[cfg(target_arch = "wasm32")]
+        {
+            app.add_system(bevy_web_resizer::web_resize_system);
+        }
         app.add_state(GameState::Game)
             .add_startup_system(camera::init)
             .add_system_to_stage(CoreStage::PostUpdate, camera::resize);
